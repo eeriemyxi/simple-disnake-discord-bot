7ac105f7aab4433aece98e0c843e1fffc8f9def7
I-am-Myxi <mail@myxi.tk>
2021-10-21 01:32:14 +0530

    new command

--- bot.py
+++ bot.py
@@ -3,18 +3,24 @@ import pkgutil
 import deta
 import dotenv
 import os
+
 dotenv.load_dotenv()
 
 
 class DisnakeSimpleBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=">", test_guilds=[858720379069136896,808030843078836254, 831582671922003969])
+        super().__init__(
+            command_prefix=">",
+            test_guilds=[858720379069136896, 808030843078836254, 831582671922003969],
+        )
         self.db_project_key = os.environ.get("DETAPROJECTKEY")
         self.server_db = deta.Deta(self.db_project_key).Base(name="serverdb")
         self.user_db = deta.Deta(self.db_project_key).Base(name="userdb")
         self.bot_db = deta.Deta(self.db_project_key).Base(name="botdb")
+
     async def on_ready(self):
         print("Ready!")
+
 
 bot = DisnakeSimpleBot()
 for cog in pkgutil.iter_modules(["cogs"]):
--- cogs/tools.py
+++ cogs/tools.py
@@ -1,10 +1,12 @@
+from logging import disable
 import disnake
 from disnake.ext import commands
 import asyncio
 from datetime import datetime, timedelta
 import time
-from utils.cog_id import COG_ID
 
+from disnake.ext.commands.core import command
+from utils.cog_id import COG_ID
 
 class Tools(commands.Cog):
     def __init__(self, bot):
@@ -52,5 +54,41 @@ class Tools(commands.Cog):
                 f"Hello <@{inter.author.id}>, I couldn't DM you so I'm reminding you here. You asked me to remind you <t:{int(created_at)}:R>. Reason: `{reason}`"
             )
         return
+    @commands.user_command(name = 'View avatar')
+    async def user_commmand_avatar(self, inter):
+        await inter.response.send_message(
+            embed = disnake.Embed(
+                title = f'Avatar of {inter.target}')
+                .set_image(url=inter.target.display_avatar.url)
+                )
+    @commands.slash_command(name = 'avatar')
+    async def slash_command_avatar(self, inter, user: disnake.Member = commands.Param(lambda inter: inter.author), ephemeral: bool = False):
+        '''
+        View your or someone else's profile picture.
+
+        Parameters
+        ----------
+        user: 
+            The user to view avatar of. Leave this parameter empty to view your own.
+        ephemeral:
+            Sends ephemeral message if set to True. Defaults to False.
+        '''
+        await inter.response.send_message(
+            embed = disnake.Embed(
+                title = f'Avatar of {user}')
+                .set_image(url=user.display_avatar.url),
+            ephemeral = ephemeral
+            )
+    @commands.command(name = 'avatar', usage='[user]')
+    async def avatar(self, ctx, user: disnake.Member = None):
+        '''
+        View your or someone else's profile picture.
+        '''
+        user = user or ctx.author
+        await ctx.send(
+            embed = disnake.Embed(
+                title = f'Avatar of {user}')
+                .set_image(url=user.display_avatar.url)
+                )
 def setup(bot):
     bot.add_cog(Tools(bot))
